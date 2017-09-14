edcbfae5b6e743ec599398fb1c06a23c6f6671b3
Shayla Richard <shaylarichard@hotmail.com>
2017-09-14 10:10:01 -0600

    second add

--- public/app.js
+++ public/app.js
@@ -1,46 +1,50 @@
 //starts new stuff//
+var form= document.getElementsByClass("pureForm");
+
+
+function init() {
+  gapi.client.setApiKey("AIzaSyA8qRL8nR8ce1gJaiFC5LHPBmo--TY5mds");
+  gapi.client.load("youtube", "v3", function() {
+    // yt api is ready
+  });
+}
 
 function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
-
-
 
  $(function() {
    $("form").on("submit",function(e) {
      e.preventDefault();
+     var enterSong = e.target.elements.search.val();
      // prepare the request
-     var request = gapi.client.youtube.search.list({
+      var request = gapi.client.youtube.search.list({
          part: "snippet",
          type: "video",
          channelId: "whitneyhoustonVEVO",
          maxResults: 10,
          order:"viewcount",
-         q: encodeURIComponent($("#search").val()).replace(/%20/g, "+")
+         q: encodeURIComponent($(enterSong).val()).replace(/%20/g, "+")
         });
         // execute the request
         request.execute(function(response) {
-          var results = response.result;
-          $("#results").html("");
+        });
+        results();
+      }
+
+      $("#results").html("");
+        var results = response.result;
           $.each(results,function(index, item) {
             $.get("item.html",function(data){
             $("#results").append(tplawesome(data,[{"title":item.snippet.title,"videoid": item.id.videoId}]));
           });
         });
-        resetVideoHeight();
-      });
+        function resetVideoHeight() {
+          $(".video").css("height", $("#results").width() * 9/16);
     });
 
     $(window).on("resize", resetVideoHeight);
 
   });
 
-      function resetVideoHeight() {
-      $(".video").css("height", $("#results").width() * 9/16);
+  resetVideoHeight();
   }
-
-    function init() {
-      gapi.client.setApiKey("AIzaSyA8qRL8nR8ce1gJaiFC5LHPBmo--TY5mds");
-      gapi.client.load("youtube", "v3", function() {
-        // yt api is ready
-      });
-
-    }
+});
